89a556abbe9bf6bddf86c5e3ad4fdd08e1523127
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-02-18 23:34:21 -0500

    print blocks, add block

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,26 @@ from block import Block
 
 class Blockchain:
     def __init__(self):
-        pass
+        self.chain = []
+        self.all_transactions = []
+        self.genesis_block()
 
     def genesis_block(self):
+        transactions = {}
+        genesis_block = Block(transactions, "0")
+        self.chain.append(genesis_block)
+        return self.chain
+
+    def print_blocks(self):
+        for i in range(len(self.chain)):
+            current_block = self.chain[i]
+            print("Block {} {}".format(i, current_block))
+            current_block.print_contents()
+
+    def add_block(self, transactions):
+        previous_block_hash = self.chain[len(self.chain) - 1].hash
+        new_block = Block(transactions, previous_block_hash)
+        self.chain.append(new_block)
+
+    def validate_chain(self):
         pass
