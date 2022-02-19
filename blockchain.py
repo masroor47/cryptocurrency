733a5d45dcd3f1284a7a2fc3d90b65cd28b73d7c
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-02-18 23:55:00 -0500

    added print to validate chain

--- blockchain.py
+++ blockchain.py
@@ -29,7 +29,11 @@ class Blockchain:
             current = self.chain[i]
             previous = self.chain[i-1]
             if current.hash != current.generate_hash():
+                print(
+                    "The current hash of the block does not equal the generated hash of the block.")
                 return False
             if current.previous_hash != previous.generate_hash():
+                print(
+                    "The previous block's hash does not equal the previous hash value stored in the current block.")
                 return False
         return True
